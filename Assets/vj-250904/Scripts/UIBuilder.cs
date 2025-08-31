f1b5f4097b197fc176e1f1e48b33aff9ea4cb158
W0NYV <w0nyv.707@gmail.com>
2025-08-31 23:26:00 +0900

    ApplicationSettings 追加

--- Assets/vj-250904/Scripts/UIBuilder.cs
+++ Assets/vj-250904/Scripts/UIBuilder.cs
@@ -13,6 +13,7 @@ namespace W0NYV.vj250904
         private Element CreateElement()
         {
             return UI.Window("VJ 250904 v0.0.0",
+                UI.WindowLauncher<ApplicationSettings>(),
                 UI.Button("Save", Prefs.Save)    
             ).SetClosable(false);
         }
