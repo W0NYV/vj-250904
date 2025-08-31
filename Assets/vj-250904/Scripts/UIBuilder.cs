3f5f65cf2d03aeb8b3a89c4bec0d67a4d0047427
W0NYV <w0nyv.707@gmail.com>
2025-08-31 23:11:04 +0900

    UI 作り始めた

---
+++ Assets/vj-250904/Scripts/UIBuilder.cs
@@ -0,0 +1,35 @@
+using PrefsGUI;
+using RosettaUI;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace W0NYV.vj250904
+{
+    [RequireComponent(typeof(RosettaUIRoot))]
+    public class UIBuilder : MonoBehaviour
+    {
+        private RosettaUIRoot _rosettaUIRoot;
+
+        private Element CreateElement()
+        {
+            return UI.Window("VJ 250904 v0.0.0",
+                UI.Button("Save", Prefs.Save)    
+            ).SetClosable(false);
+        }
+
+        private void Start()
+        {
+            TryGetComponent(out _rosettaUIRoot);
+            
+            _rosettaUIRoot.Build(CreateElement());
+        }
+
+        private void Update()
+        {
+            if (Keyboard.current[Key.D].wasPressedThisFrame)
+            {
+                _rosettaUIRoot.enabled = !_rosettaUIRoot.enabled;
+            }
+        }
+    }   
+}
