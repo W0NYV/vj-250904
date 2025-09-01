5203432761458d821b32f1e3e47e87b1f62ba0b7
W0NYV <w0nyv.707@gmail.com>
2025-09-01 21:36:18 +0900

    MIDI 対応

---
+++ Assets/vj-250904/Scripts/DeviceMidiReceiver.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+
+namespace W0NYV.vj250904
+{
+    public class DeviceMidiReceiver : MonoBehaviour
+    {
+        public static UnityEvent<(int id, float value)> OnMidiControlChangeReceived { get; } = new();
+        public static UnityEvent<(int id, float value)> OnMidiNoteReceived { get; } = new();
+
+        [SerializeField] private bool _isDebug;
+        
+        private void OnEnable()
+        {
+            InputSystem.onDeviceChange += (device, change) =>
+            {
+                if (change != InputDeviceChange.Added) return;
+
+                var midiDevice = device as Minis.MidiDevice;
+                if (midiDevice == null) return;
+
+                midiDevice.onWillControlChange += (control, velocity) =>
+                {
+                    OnMidiControlChangeReceived.Invoke((control.controlNumber, velocity));
+                    
+                    if (!_isDebug) return;
+
+                    Debug.Log("CC :" + control.controlNumber + " :" + velocity);
+                };
+                
+                midiDevice.onWillNoteOn += (note, velocity) => {
+                    
+                    OnMidiNoteReceived.Invoke((note.noteNumber, 1f));
+                    
+                    if (!_isDebug) return;
+                    
+                    // Note that you can't use note.velocity because the state
+                    // hasn't been updated yet (as this is "will" event). The note
+                    // object is only useful to specify the target note (note
+                    // number, channel number, device name, etc.) Use the velocity
+                    // argument as an input note velocity.
+                    Debug.Log(string.Format(
+                        "Note On #{0} ({1}) vel:{2:0.00} ch:{3} dev:'{4}'",
+                        note.noteNumber,
+                        note.shortDisplayName,
+                        velocity,
+                        (note.device as Minis.MidiDevice)?.channel,
+                        note.device.description.product
+                    ));
+                };
+
+                midiDevice.onWillNoteOff += (note) => {
+
+                    OnMidiNoteReceived.Invoke((note.noteNumber, 0f));
+                    
+                    if (!_isDebug) return;
+                    
+                    Debug.Log(string.Format(
+                        "Note Off #{0} ({1}) ch:{2} dev:'{3}'",
+                        note.noteNumber,
+                        note.shortDisplayName,
+                        (note.device as Minis.MidiDevice)?.channel,
+                        note.device.description.product
+                    ));
+                };
+            };
+        }
+    }
+}
---
+++ Assets/vj-250904/Scripts/MidiAssigner.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace W0NYV.vj250904
+{
+    [System.Serializable]
+    public class MidiAssignInfo
+    {
+        public int ID;
+        public UnityEvent<float> OnReceived = new();
+    }
+    
+    public class MidiAssigner : MonoBehaviour
+    {
+        [SerializeField] private MidiAssignInfo[] _noteAssigns;
+        [SerializeField] private MidiAssignInfo[] _controlChangeAssigns;
+        
+        private void Awake()
+        {
+            DeviceMidiReceiver.OnMidiNoteReceived.AddListener(x =>
+            {
+                foreach (var assignInfo in _noteAssigns)
+                {
+                    if (assignInfo.ID == x.id)
+                    {
+                        assignInfo.OnReceived.Invoke(x.value);
+                    }
+                }
+            });
+            
+            DeviceMidiReceiver.OnMidiControlChangeReceived.AddListener(x =>
+            {
+                foreach (var assignInfo in _controlChangeAssigns)
+                {
+                    if (assignInfo.ID == x.id)
+                    {
+                        assignInfo.OnReceived.Invoke(x.value);
+                    }
+                }
+            });
+        }
+    }
+}
